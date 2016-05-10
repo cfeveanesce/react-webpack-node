918e9c4bf94546ad578a7c15aaefd5c41b8db19e
cfeveanesce <cfevanesce@hotmail.com>
2016-05-10 17:38:41 -0400

    sideBar jsx file

---
+++ app/components/sideBar.jsx
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react';
+import classNames from 'classnames/bind';
+import styles from 'css/components/sideBar';
+
+const cx = classNames.bind(styles);
+
+const SideBar = () => {
+    return (
+        <div className={cx('productList')}>
+
+        </div>
+    );
+};
+
+export default SideBar;
+
